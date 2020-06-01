f2cbcb3da3880e809897a8b14ed7eb7ee2c06863
callelacouture <clacouture@klru.org>
2020-05-31 22:59:11 -0500

    texas mutual logo

--- src/components/LatestNews/TexasMutual.js
+++ src/components/LatestNews/TexasMutual.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import TexasMutualLogo from '../../images/TexasMutual.jpg'
+import TexasMutualLogo from '../../images/texasmutualLarge.jpg'
 //import {Link} from 'gatsby'
 import styled from 'styled-components'
 import { OutboundLink } from 'gatsby-plugin-gtag'
@@ -26,7 +26,7 @@ const TexasMutualDiv = styled.div`
       border: 1px solid rgba(0, 57, 70, .25);
       box-shadow: 0px 2px 3px rgba(0, 57, 70, .1);
       align-items: center;
-      grid-template-columns: 30% 70%;
+      //grid-template-columns: 30% 70%;
       padding:10px;
       margin-top: 10px;
       h4{
@@ -58,17 +58,19 @@ const TexasMutualDiv = styled.div`
   }
 `
 const TexasMutual = (props) =>{
-  const sponsorList = props;
-  const sponsorships = sponsorList.sponsors;
+  //const sponsorList = props;
+  //const sponsorships = sponsorList.sponsors;
   return(
     <TexasMutualDiv>
       <div>
        <h2>Decibel is Sponsored By:</h2>
-       {sponsorships.map((sponsorship)=>(
+       {/* {sponsorships.map((sponsorship)=>(
            <OutboundLink href={`${sponsorship.sponsorLink}`}><div className='sponsorDiv' key={sponsorship.id}>
        <img src={`https:${sponsorship.image.fluid.src}`} alt='TMLogo' className='logoImage' />
          <h4>{sponsorship.sponsorTitle}</h4></div></OutboundLink>
-       ))}
+       ))} */}
+       <OutboundLink href="https://www.texasmutual.com/"><div className='sponsorDiv'><img src={TexasMutualLogo} alt='TMLogo' className='logoImage' /></div></OutboundLink>
+
       <h2>Support Decibel</h2>
       <button><OutboundLink href="https://austinpbs.org/donate">Donate</OutboundLink></button>
       {/* <button><OutboundLink href="http://bycell.co/wjtf">Donate</OutboundLink></button> */}
